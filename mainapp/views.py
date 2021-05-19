c989785ce10be8f6f1c9268a9c11c940cda7abcf
Ilya <forwow21@mail.ru>
2021-05-19 12:00:15 +0300

    19.05.2021 update_4

--- mainapp/views.py
+++ mainapp/views.py
@@ -46,7 +46,11 @@ class UserUpdateView(generics.UpdateAPIView):
 
     def update(self, request, *args, **kwargs):
         user = User.objects.get(id=kwargs['pk'])
-        if user.other_invite_code:
+        token_user = Token.objects.get(user=user)
+        token_request = request.META.get("HTTP_TOKEN", "")
+        if token_user != token_request:
+            raise ValidationError('This is not your profile')
+        elif user.other_invite_code:
             raise ValidationError('Code already exists')
         elif request.data['other_invite_code'] == user.self_invite_code:
             raise ValidationError('This code is yours')
